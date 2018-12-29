67909cf7bc645e81d4ac8ab178fd64f7f7a2b441
liguanglei <liguanglei@boyue.com>
2018-12-29 13:50:59 +0800

    特
    特

--- GlShowPhoto/GlComHeader.h
+++ GlShowPhoto/GlComHeader.h
@@ -8,8 +8,6 @@
 
 #ifndef GlComHeader_h
 #define GlComHeader_h
-#import "Masonry.h"
-#import "UIImageView+WebCache.h"
 
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
